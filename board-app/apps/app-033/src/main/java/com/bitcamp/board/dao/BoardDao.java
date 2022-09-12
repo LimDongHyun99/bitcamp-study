be617b2ea55f2d6e0fce3574a26d171597f3d1e4
LimDongHyun99 <ldh9036@naver.com>
2022-09-12 15:59:18 +0900

    백업46

--- board-app/apps/app-033/src/main/java/com/bitcamp/board/dao/BoardDao.java
+++ board-app/apps/app-033/src/main/java/com/bitcamp/board/dao/BoardDao.java
@@ -2,6 +2,8 @@ package com.bitcamp.board.dao;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -12,131 +14,33 @@ import com.bitcamp.board.domain.Board;
 public class BoardDao {
 
   List<Board> list = new LinkedList<>();
-
   private int boardNo = 0;
   String filename;
 
+  public BoardDao(String filename) {
+    this.filename = filename;
+  }
+
+
   public void load() throws Exception {
-    FileInputStream in = new FileInputStream(filename);
-
-    int size = (in.read() << 24) + (in.read() << 16) + (in.read() << 8) + in.read();
-
-    for (int i = 0; i < size; i++) {
-      Board board = new Board();
-
-      int value = 0;
-      value += in.read() << 24;
-      value += in.read() << 16;
-      value += in.read() << 8;
-      value += in.read();
-      board.no = value;
-
-      int len = 0;
-
-      len = (in.read() << 24) + (in.read() << 16) + (in.read() << 8) + in.read();
-
-      byte[] bytes = new byte[len];
-
-      in.read(bytes);
-
-      board.title = new String(bytes, "UTF-8");
-
-      // 게시글 내용 읽기
-      len = (in.read() << 24) + (in.read() << 16) + (in.read() << 8) + in.read();
-      bytes = new byte[len];
-      in.read(bytes);
-      board.content = new String(bytes, "UTF-8");
-
-      // 게시글 작성자 읽기
-      len = (in.read() << 24) + (in.read() << 16) + (in.read() << 8) + in.read();
-      bytes = new byte[len];
-      in.read(bytes);
-      board.writer = new String(bytes, "UTF-8");
-
-      // 게시글 암호 읽기
-      len = (in.read() << 24) + (in.read() << 16) + (in.read() << 8) + in.read();
-      bytes = new byte[len];
-      in.read(bytes);
-      board.password = new String(bytes, "UTF-8");
-
-      // 게시글 조회수 읽기
-      board.viewCount = (in.read() << 24) + (in.read() << 16) + (in.read() << 8) + in.read();
-
-      // 게시글 등록일 읽기
-      board.createdDate = (((long) in.read()) << 56) + (((long) in.read()) << 48)
-          + (((long) in.read()) << 40) + (((long) in.read()) << 32) + (((long) in.read()) << 24)
-          + (((long) in.read()) << 16) + (((long) in.read()) << 8) + ((in.read()));
-
-
-
-      list.add(board);
-      boardNo = board.no;
-
+    try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename))) {
+      int size = in.readInt();
+      for (int i = 0; i < size; i++) {
+        Board board = (Board) in.readObject();
+        list.add(board);
+        boardNo = board.no;
+      }
     }
-    in.close();
-
   }
 
   public void save() throws Exception {
-    FileOutputStream out = new FileOutputStream(filename);
+    try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filename))) {
+      out.writeInt(list.size());
+      for (Board board : list) {
+        out.writeObject(board);
 
-    out.write(list.size() >> 24);
-    out.write(list.size() >> 16);
-    out.write(list.size() >> 8);
-    out.write(list.size());
-
-    for (Board board : list) {
-
-      out.write(board.no >> 24);
-      out.write(board.no >> 16);
-      out.write(board.no >> 8);
-      out.write(board.no);
-
-      byte[] bytes = board.title.getBytes("UTF-8");
-      out.write(bytes.length >> 24);
-      out.write(bytes.length >> 16);
-      out.write(bytes.length >> 8);
-      out.write(bytes.length);
-      out.write(bytes);
-
-      bytes = board.content.getBytes("UTF-8");
-      out.write(bytes.length >> 24);
-      out.write(bytes.length >> 16);
-      out.write(bytes.length >> 8);
-      out.write(bytes.length);
-      out.write(bytes);
-
-      bytes = board.writer.getBytes("UTF-8");
-      out.write(bytes.length >> 24);
-      out.write(bytes.length >> 16);
-      out.write(bytes.length >> 8);
-      out.write(bytes.length);
-      out.write(bytes);
-
-      bytes = board.password.getBytes("UTF-8");
-      out.write(bytes.length >> 24);
-      out.write(bytes.length >> 16);
-      out.write(bytes.length >> 8);
-      out.write(bytes.length);
-      out.write(bytes);
-
-      out.write(board.viewCount >> 24);
-      out.write(board.viewCount >> 16);
-      out.write(board.viewCount >> 8);
-      out.write(board.viewCount);
-
-      out.write((int) (board.createdDate >> 56));
-      out.write((int) (board.createdDate >> 48));
-      out.write((int) (board.createdDate >> 40));
-      out.write((int) (board.createdDate >> 32));
-      out.write((int) (board.createdDate >> 24));
-      out.write((int) (board.createdDate >> 16));
-      out.write((int) (board.createdDate >> 8));
-      out.write((int) (board.createdDate));
+      }
     }
-
-    out.close();
-
   }
 
   public void insert(Board board) {
